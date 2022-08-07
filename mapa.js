a3ab0b4e899dccef421475ca8ef08129977f4783
David Alvarado <david.alvaradolb@gmail.com>
2022-08-08 01:23:29 +0200

    alisonybenja

---
+++ mapa.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:5.1850572,lng:-77.135898};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
